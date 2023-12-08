a2a7f49f3ee39c0be09c270f8d9812e049e3d370
Saleh <dev@printingtalks.ae>
2023-12-08 15:38:48 +0400

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
