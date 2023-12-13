bed7047a7d99a3dd5dbcb4e858b96615e10a9cc1
Saleh <dev@printingtalks.ae>
2023-12-13 16:46:00 +0400

    Minor styling adjustments

--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -24,8 +24,8 @@ const ProductsList: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const categoryMapping = {
     "food": "Food Packaging",
@@ -48,14 +48,19 @@ const ProductsList: React.FC = () => {
   };
 
   useEffect(() => {
-    const filtered = products.filter(product =>
-      selectedCategories.length === 0 ||
-      product.category.some(cat => selectedCategories.includes(cat))
-    );
-    setFilteredProducts(filtered);
+    try {
+      const filtered = products.filter(product =>
+        selectedCategories.length === 0 ||
+        product.category.some(cat => selectedCategories.includes(cat))
+      );
+      setFilteredProducts(filtered);
+    }
+    catch { console.error("Error filtering:", error); }
+
   }, [products, selectedCategories]);
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchProducts = async () => {
       try {
         const productsResponse = await client.queries.productConnection();
@@ -65,27 +70,27 @@ const ProductsList: React.FC = () => {
         console.error('Error fetching products:', error);
         setError('Error fetching products. Please try again later.');
       } finally {
-        setLoading(false);
+        setIsLoading(false);
       }
     };
 
     fetchProducts();
   }, []);
 
-  if (loading) return
-  <div className='w-[100%] grid place-items-center h-[367px]'>
-    <span className="loading loading-spinner loading-lg"></span>
-  </div>;
-  if (error) return <p>{error}</p>;
-
   const categoryLabels = Object.keys(categoryMapping);
   const categoryCheckboxes = categoryLabels.map((label, index) => (
     <input key={index} id={label} onChange={(e) => handleCategoryChange(label, e.target.checked)} type="checkbox" aria-label={label.charAt(0).toUpperCase() + label.slice(1)} className="btn btn-ghost btn-accent btn-sm font-medium w-fit" />
   ));
 
+
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <>
-      <div className="sm:hidden sticky top-0 flex overflow-auto px-4 py-8 gap-4 outline outline-1 outline-gray-300 backdrop-blur-2xl">
+      <div className="sm:hidden sticky top-0 flex overflow-auto px-4 py-8 gap-4 outline outline-1 outline-gray-300 backdrop-blur-2xl z-30">
         {categoryCheckboxes}
       </div>
       <div className="search-results-container flex p-2 md:px-12 md:py-8">
@@ -95,7 +100,10 @@ const ProductsList: React.FC = () => {
           <p className='text-xl font-medium mb-2'>Categories</p>
           {categoryCheckboxes}
         </aside>
-        <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 md:gap-6 min-h-[12rem] w-full">
+        {isLoading &&
+          <span className="loading loading-spinner loading-lg"></span>
+        }
+        <div className={` ${isLoading && "invisible"} grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 md:gap-6 min-h-[12rem] w-full`}>
           {filteredProducts.map((product, index) => (
             <ProductCard key={index} href={product.slug} title={product.productName} desc={product.productDescription} image={product.imageUrls.image1} />
           ))}
