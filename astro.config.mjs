fa8ffc09e0983d915b9da5c7701215a0b46189c6
Saleh <dev@printingtalks.ae>
2023-12-15 13:55:42 +0400

    Added vercel web analytics and speed insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 import react from "@astrojs/react";
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel()
+  output: 'server',
+  adapter: vercel({
+    speedInsights: {
+      enabled: true,
+    },
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
